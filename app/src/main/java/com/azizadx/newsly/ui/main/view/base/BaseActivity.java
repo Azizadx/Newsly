c1195fedd28ae667458fb5e014e753038024c847
mekedi-mkt <mekedi.mkt@gmail.com>
2021-08-07 20:39:39 +0300

    splash screen user authentication

--- app/src/main/java/com/azizadx/newsly/ui/main/view/base/BaseActivity.java
+++ app/src/main/java/com/azizadx/newsly/ui/main/view/base/BaseActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.azizadx.newsly.R;
+import com.azizadx.newsly.ui.main.view.SignUpActivity;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -32,6 +33,12 @@ public class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         auth = FirebaseAuth.getInstance();
+    }
+
+    public Intent authUser() {
+//        return auth == null ? new Intent (this, SignInActivity.class)
+//                : new Intent (this, HomeFeedActivity.class);
+        return new Intent (this, SignUpActivity.class);
     }
 
     public void signUp(String email, String password) {
@@ -103,6 +110,7 @@ public class BaseActivity extends AppCompatActivity {
                         // Sign in success, update UI with the signed-in user's information
                         FirebaseUser user = auth.getCurrentUser();
                         auth.updateCurrentUser(user);
+                        Toast.makeText(getApplicationContext(), "signIn successful", Toast.LENGTH_SHORT).show();
                     } else {
                         // If sign in fails, display a message to the user.
                         Toast.makeText(getApplicationContext(), "signInWithCredential:failure", Toast.LENGTH_SHORT).show();
