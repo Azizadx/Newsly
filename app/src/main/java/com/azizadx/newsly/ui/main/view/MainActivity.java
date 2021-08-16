2c82b47fc5650da5aad2bc8172bc71bdfa863bc9
Azizadx <nasrallahh55@gmail.com>
2021-08-17 00:58:51 +0300

    webview for ArticlePage is implement

--- app/src/main/java/com/azizadx/newsly/ui/main/view/MainActivity.java
+++ app/src/main/java/com/azizadx/newsly/ui/main/view/MainActivity.java
@@ -46,7 +46,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
-                if(tab.getPosition()==0){
+                if(tab.getPosition()==0||tab.getPosition()==1||tab.getPosition()==2
+                        ||tab.getPosition()==3||tab.getPosition()==4||
+                        tab.getPosition()==5||tab.getPosition()==6){
                     pagerAdapter.notifyDataSetChanged();
                 }
             }
